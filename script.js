c09a668aceca7f5623a1c0517ef795e851800909
emmanuel <edamilare98@gmail.com>
2023-11-20 21:13:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"]
+for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+
+    if (currentName.charAt(0).toLowerCase() === 'j') {
+        console.log(`Goodbye ${currentName}`);
+    }else{
+        console.log(`Hello ${currentName}`);
+    }
+    
+}
+
+
+
